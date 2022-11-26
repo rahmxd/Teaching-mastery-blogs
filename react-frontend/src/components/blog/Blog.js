21413706c73a41bae3455ac9aedbf4f5c073a947
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-11-26 15:06:19 +0000

    passing tests Blog rendering component (renders Title, Content, Edit and Delete features)

--- react-frontend/src/components/blog/Blog.js
+++ react-frontend/src/components/blog/Blog.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Card, CardBody, CardTitle } from 'reactstrap'
+import React from 'react';
+import { Button, Card, CardBody, CardText, CardTitle, Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-const Blog = () => {
+const Blog = ({blog}) => {
   return (
     <Card body inverse color="info">
         <CardBody>
             <CardTitle className="font-weight-bold">
-
+                {blog.title}
             </CardTitle>
+            <CardText>
+                {blog.content}
+            </CardText>
+            <Container>
+                <Link className="btn btn-primary" to="/update">
+                    Edit
+                </Link>
+                <Button>
+                    Delete
+                </Button>
+            </Container>
         </CardBody>
     </Card>
   )
---
+++ react-frontend/src/components/blog/__test__/Blog.test.js
@@ -0,0 +1,53 @@
+import '@testing-library/jest-dom'
+import { render, screen, cleanup } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Blog from '../Blog';
+import blogData from './testBlog.json';
+
+const MockBlog = ({blogData}) => {
+    return (
+        <BrowserRouter>
+            <Blog blog={blogData}/>
+        </BrowserRouter>
+    )
+}
+
+describe("Blog unit tests", () => {
+    
+    
+    afterEach(()=>{
+        cleanup();
+    })
+
+    it('should render the Card Title Element with the title of the Blog', async() => {
+        render(<MockBlog blogData={blogData}/>)
+        const CardTitleElement = screen.getByText(/New Blog/i);
+        expect(CardTitleElement).toBeInTheDocument();
+
+    })
+
+    it('should render the Card Text Content Element with the content of the Blog', async() => {
+        
+        render(<MockBlog blogData={blogData}/>)
+        const CardTextContentElement = screen.getByText(/blah blah blah/i);
+        expect(CardTextContentElement).toBeInTheDocument();
+
+    })
+
+    it('should render the Edit link', async() => {
+        
+        render(<MockBlog blogData={blogData}/>)
+        const EditLinkElement = screen.getByText(/Edit/i);
+        expect(EditLinkElement).toBeInTheDocument();
+    })
+
+    it('should render the Delete Button', async() => {
+        
+        render(<MockBlog blogData={blogData}/>)
+        const DeleteButtonElement = screen.getByText(/Delete/i);
+        expect(DeleteButtonElement).toBeInTheDocument();
+    })
+
+
+
+})
